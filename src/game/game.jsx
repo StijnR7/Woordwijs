5fbe773ba65f230b4252c0006996c37f661abd82
Wschiks <1206720@student.roc-nijmegen.nl>
2025-05-26 23:28:27 +0200

    De style van game.jsx aangepast naar ons design. Als je fout andtwoord fout melding. Na een submit rest in input. Na submit check of input een van de goude woorden bezit ook als gebruiker meerdere woorden in gevoerd heeft.

--- src/game/game.jsx
+++ src/game/game.jsx
@@ -1,68 +1,79 @@
-
- 
-  import { useState, useEffect } from 'react'
+import {useState, useEffect} from 'react'
 
 import '../App.css'
-import {getDocs, collection, doc, addDoc, deleteDoc} from 'firebase/firestore'
-import { db } from '../config/firebase';
-import Admin from '../admin/admin';
-import {
-  Router,
-  Routes,
-  Route,
-  Link
-} from "react-router-dom";
+import {getDocs, collection} from 'firebase/firestore'
+import {db} from '../config/firebase';
 
 
 function Game() {
- const [AllWords, setAllWords] = useState([])
-  const [WordToDescribe, setWordToDescribe] = useState({
-    Word: "hoi",
-    Descriptions: ["groet", "hallo"]
+    const [AllWords, setAllWords] = useState([])
+    const [WordToDescribe, setWordToDescribe] = useState({
+        Word: "hoi",
+        Descriptions: ["groet", "hallo"]
 
 
-  })
+    })
 
 
-  useEffect(() => {
-    const getWords = async () => {
-      const data = await getDocs(collection(db, "Words"))
-      setAllWords(data.docs.map((doc) => ({...doc.data(), id: doc.id})));
-      
-     
+    useEffect(() => {
+        const getWords = async () => {
+            const data = await getDocs(collection(db, "Words"))
+            setAllWords(data.docs.map((doc) => ({...doc.data(), id: doc.id})));
+
+
+        }
+
+        getWords();
+
+    }, [])
+
+    const CheckIfCorrect = (e) => {
+
+
+        e.preventDefault();
+        if (e.target.chosenWord.value) {
+            const inputWords = e.target.chosenWord.value.toLowerCase().split(/\s+/);
+            if (inputWords.some(word => WordToDescribe.Descriptions.includes(word))) {
+                setWordToDescribe(AllWords[Math.floor(Math.random() * AllWords.length)]);
+                alert("goed");
+            } else {
+                alert("fout");
+            }
+        }
+        document.getElementById("WordInput").value="";
     }
-    
-    getWords();
+    return (
+        <>
+            <div className="headers bg-white w-screen ">
+                <img src="src/img/Logo van ROC-Nijmegen.svg" alt="RocLogo" className="roclogo"/>
+            </div>
 
-  }, [])
+            <div className="diagonal-split">
+                <h1 className="roct">Leg het volgende woord uit:</h1>
 
-  const CheckIfCorrect = (e) => {
-    
-    e.preventDefault();
-    if(WordToDescribe.Descriptions.includes(e.target.chosenWord.value)){
-      setWordToDescribe(AllWords[Math.floor(Math.random() * AllWords.length)]);
-      alert("goed");  
-      
-    }
-    
+                <div className="wordkaart text-center">
+                    <h2 id="WordToDescribe" className="roct">{WordToDescribe.Word}</h2>
+                </div>
+
+                <form onSubmit={CheckIfCorrect}>
+
+                    <input
+                        className="text-white"
+                        type="text"
+                        name="chosenWord"
+                        id="WordInput"
+                        placeholder="Beschrijf het woord"
+                    />
+                    <div className="margintop">
+                    <button type="submit">Guess</button>
+                    </div>
+                </form>
 
 
-  }
-  return (
-    <>
-    
-     <div id="GameContainer">
-      <div id="WordContainer">
-        <p id="WordToDescribe">{WordToDescribe.Word}</p>
-      </div>
-      <form onSubmit={CheckIfCorrect}>
-        <input type="text" name="chosenWord" id="WordInput" />
-        <button type="submit">Guess</button>
+            </div>
+        </>
+    );
 
-      </form>
-     </div>
-    </>
-  )
 }
 
 export default Game
